960011953d6af45a4b64c0a56a1a8cbde8bda478
hedasus <sushilheda2089@gmail.com>
2021-02-25 17:32:23 +0530

    Repo changes

--- account-opening/account-opening/src/main/java/com/gcp/entity/AccountEntity.java
+++ account-opening/account-opening/src/main/java/com/gcp/entity/AccountEntity.java
@@ -3,23 +3,32 @@ package com.gcp.entity;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 
 @Entity
 @Getter
 @Setter
+@Table (name="USER_ACCOUNT")
 public class AccountEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column (name ="account_number")
     private Long acctNo;
+
+    @Column (name ="name")
     private String name;
+
+    @Column (name ="contact")
     private String contactNo;
+
+    @Column (name ="address")
     private String address;
+
+    @Column (name ="pan_tan_no")
     private String panTanNo;
+
+    @Column (name ="email_id")
     private String emailId;
 }
