5f9bfd92df41294e4faf0e96fa276dd60b52360f
hedasus <sushilheda2089@gmail.com>
2021-02-25 17:25:30 +0530

    Repo changes

--- account-opening/account-opening/src/main/java/com/gcp/controller/AccountController.java
+++ account-opening/account-opening/src/main/java/com/gcp/controller/AccountController.java
@@ -9,10 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @EnableAutoConfiguration
@@ -26,8 +23,7 @@ public class AccountController {
     private ModelMapper modelMapper;
 
     @PostMapping(path = "/accountOpen")
-    @ResponseBody
-    public ResponseEntity<AccountOpenResponse> createAccount(AccountOpenRequest request) {
+    public ResponseEntity<AccountOpenResponse> createAccount(@RequestBody AccountOpenRequest request) {
         AccountEntity entity =  service.updateAccount(request);
 // convert entity to DTO
         AccountOpenResponse response = modelMapper.map(entity, AccountOpenResponse.class);
