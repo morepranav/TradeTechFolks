3fb0fe8ac594b0b1593a2cf7a84623d878ef9b32
hedasus <sushilheda2089@gmail.com>
2021-02-25 17:49:08 +0530

    Repo changes1

--- account-opening/account-opening/src/main/java/com/gcp/account/AccountOpeningApplication.java
+++ account-opening/account-opening/src/main/java/com/gcp/account/AccountOpeningApplication.java
@@ -3,6 +3,7 @@ package com.gcp.account;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -10,6 +11,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableAutoConfiguration
 @EnableJpaRepositories("com.gcp.repository")
 @ComponentScan
+@EntityScan("com.gcp.entity")
 public class AccountOpeningApplication {
 
 	public static void main(String[] args) {
